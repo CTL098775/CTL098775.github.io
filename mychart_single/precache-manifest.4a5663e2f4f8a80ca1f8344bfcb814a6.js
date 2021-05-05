3de35cc270153dbd13493d995cbe6858d3431eb0
CTL098775 <j747300k@gmail.com>
2021-05-06 01:45:55 +0800

    66

---
+++ mychart_single/precache-manifest.4a5663e2f4f8a80ca1f8344bfcb814a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5489bb07ef8e844c911e77987d99adca",
+    "url": "/mychart_single/big-data.svg"
+  },
+  {
+    "revision": "716b35ab45c9409d7f21",
+    "url": "/mychart_single/css/app.b09c5dc2.css"
+  },
+  {
+    "revision": "12fc2c8baeeb8a9e3f65",
+    "url": "/mychart_single/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "ae02b1c5b036c70a0131846f801c5f84",
+    "url": "/mychart_single/index.html"
+  },
+  {
+    "revision": "716b35ab45c9409d7f21",
+    "url": "/mychart_single/js/app.ce6f3819.js"
+  },
+  {
+    "revision": "12fc2c8baeeb8a9e3f65",
+    "url": "/mychart_single/js/chunk-vendors.33873b8b.js"
+  },
+  {
+    "revision": "65fe7b214cf992ec3e373c3bdf5672a7",
+    "url": "/mychart_single/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/mychart_single/robots.txt"
+  }
+]);
